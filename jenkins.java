5f7ca79a124d27e3e34c492d4c8d90b3e0e6bf7b
banty <banteesharma4848@gmail.com>
2022-01-16 15:43:04 +0530

    added new changes

--- jenkins.java
+++ jenkins.java
@@ -1,6 +1,9 @@
 class jenkins{
 public static void main(String []args){
 for (int i=0;i<10;i++)
-System.out.println(i);
+System.out.print(" " +i);
+System.out.println();
+for(int j=10;j<19;j++)
+System.out.print(" " +j);
 }
 }
